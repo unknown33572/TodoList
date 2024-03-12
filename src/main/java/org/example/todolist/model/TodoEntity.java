384b7ec9c35bebef1db12114060d0bea145f783b
S <unknown33572@gmail.com>
2024-03-12 21:23:13 +0900

    Entity

---
+++ src/main/java/org/example/todolist/model/TodoEntity.java
@@ -0,0 +1,17 @@
+package org.example.todolist.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoEntity {
+  private String id;
+  private String userId;
+  private String title;
+  private boolean done;
+}
